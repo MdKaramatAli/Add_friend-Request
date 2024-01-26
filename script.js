e07ff1ea2c00dbbffa7a6da65dc278234c289706
MD KARAMAT ALI <93656306+MdKaramatAli@users.noreply.github.com>
2024-01-26 18:30:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,4 @@
-// const addfrient = document.getElementById("add");
-// let check = 0;
-// addfrient.addEventListener("click", function()
-// {
-//     if(check == 0)
-//     {
-//         let text = document.querySelector("p");
-//         text.style.color = "green";
-//         text.innerHTML = "Now you are friend !"
-//         const friend = document.getElementById("add");
-//         friend.innerHTML = "Friends"
-//         check = 1;
-//     }
-// })
 
-// const remove_friend = document.getElementById("remove");
-// remove_friend.addEventListener("click", function()
-// {
-//     if(check  == 1)
-//     {
-//         let text = document.querySelector("p");
-//         text.style.color = "red";
-//         text.innerHTML = "Now you are not friend !"
-//         const friend = document.getElementById("add");
-//         friend.innerHTML = "Add freinds"
-//         check = 0;
-//     }
-// })
-
-
-
-// using one button
 const addfrient = document.getElementById("add");
 let check = 0;
 addfrient.addEventListener("click", function()
